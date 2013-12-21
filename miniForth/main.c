45e1e8e6b35932805bd11ef0c57c089dcaaa1043
Cao Li <tsowly@hotmail.com>
2013-12-21 21:40:10 +0800

    Initial Commit

---
+++ miniForth/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  miniForth
+//
+//  Created by Cao Li on 13-12-21.
+//  Copyright (c) 2013年 Cao Li. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
